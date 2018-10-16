164f14c96936500e8276639e73cc5b036e238142
Aaron Shim <aaron.k.shim@gmail.com>
2018-10-16 01:23:43 -0400

    Interaction model and better intent scraper.

--- predictions/scraper/scraper.py
+++ predictions/scraper/scraper.py
@@ -1,13 +1,29 @@
-import requests
-import json
+from requests import request
+from json import loads
+from itertools import combinations
+from random import sample
 
 # from IPython.core.debugger import Tracer; debug_here = Tracer()
 
-all_markets = requests.request('GET', 'https://www.predictit.org/api/marketdata/all/')
-all_markets = json.loads(all_markets.content)
-with open('predictions.csv', 'w') as f:
-    for market in all_markets['markets']:
-        f.write(market['name'].replace(",","") + "\n")
-
-
 # https://www.predictit.org/api/marketdata/markets/3633
+
+all_markets = request('GET', 'https://www.predictit.org/api/marketdata/all/')
+all_markets = loads(all_markets.content)
+
+common_words = []
+with open('common_words.txt', 'r') as f:
+    common_words = set(f.read().splitlines())
+
+with open('predictions.csv', 'w') as f:
+    keywords = set() # Sample of some keyword utterances we want Alexa to learn.
+    for market in all_markets['markets']:
+        full_title = market['name'].replace(',','').replace('?','').replace('"','').lower()
+        keywords.add(full_title)
+        # Some subset of the words in the title that are relevant.
+        relevant_words = [x for x in full_title.split() if not x in common_words]
+        for i in range(1, len(relevant_words)):
+            # 2 is the magic number because any more and we could overrun the sample size.
+            keywords.update(sample(list(' '.join(list(x)) for x in combinations(relevant_words, i)), 2))
+        # debug_here()
+    for invocation in keywords:
+        f.write(invocation + ",\n")
